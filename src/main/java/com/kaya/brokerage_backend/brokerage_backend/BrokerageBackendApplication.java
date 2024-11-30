991a376feb66a8fd59128cd024261d9860bda5dd
Muhittinkaya <mhttnkaya@gmail.com>
2024-11-30 18:11:28 +0300

    This is the first commit after spring initializer

---
+++ src/main/java/com/kaya/brokerage_backend/brokerage_backend/BrokerageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kaya.brokerage_backend.brokerage_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokerageBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaya/brokerage_backend/brokerage_backend/BrokerageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaya.brokerage_backend.brokerage_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
